d7079f7310aecde6a52dd77ea89433afbe5232f1
pshah <contactshahpriyank@gmail.com>
2018-06-30 20:25:32 -0700

    Course inserted

---
+++ test/courseinsert.js
@@ -0,0 +1,79 @@
+var assert = require('assert');
+const CourseModel = require('C:/Users/Priyank Shah/Downloads/mongodb-playlist/model/courses');
+const mongoose = require('mongoose');
+// Describe our tests
+describe('Course Insertion Testing', function(){
+
+//Drop the characters collection before each test
+this.timeout(5000);
+beforeEach(function(done){
+
+
+  //Drop the collection
+  mongoose.connection.collections.courses.drop(function(){
+    done();
+
+  });
+});
+
+
+  it('Saves a course record to the database. Testing for Books lenght', function(done){
+
+    const cour = new CourseModel({
+      Name: 'Javascript',
+      Instructor: 'M Hoffman',
+      Books:
+      [
+
+      {Title:'Age of words',Pages:300,Author:[{ Name:"Frank Carl",Age:30 },{ Name:"Mario Cedra" ,Age:40 }]},
+      {Title:'Freedom to die',Pages:300,Author:[{ Name:"Neil Carl",Age:30 },{ Name:"Mark Hunt" ,Age:40 }]}
+
+      ]
+    });
+
+    cour.save().then(function(){
+      
+       CourseModel.findOne({Name:'Javascript'}).then(function(result){
+
+        assert(result.Books.length === 2)
+          done();
+      });
+
+
+    });
+
+  });
+
+
+
+  it('Saves a course record to the database. Testing for the name of the author', function(done){
+
+    const cour = new CourseModel({
+      Name: 'Java',
+      Instructor: 'M Hoffman',
+      Books:
+      [
+
+      {Title:'Age of words',Pages:300,Author:[{ Name:"Frank Carl",Age:30 },{ Name:"Mario Cedra" ,Age:40 }]},
+      {Title:'Freedom to die',Pages:300,Author:[{ Name:"Neil Carl",Age:30 },{ Name:"Mark Hunt" ,Age:40 }]}
+
+      ]
+    });
+
+    cour.save().then(function(){
+      
+       CourseModel.findOne({Name:'Javascript'}).then(function(result){
+
+        assert(result.Books.Title === 'Age of words' && result.Books.Author.Name === 'Frank Carl')
+          done();
+      });
+
+
+    });
+
+  });
+
+
+
+
+});
